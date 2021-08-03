46ff3a35918e38226a9c6f36b2743da6bb215d4c
Luiz Felipe Limão <lflimao@hotmail.com>
2021-08-03 17:40:30 -0300

    Update main.go

--- main.go
+++ main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"fmt"
-	"training/app"
 )
 
 func main() {
 	array := []int{-4, 3, -9, 0, 4, 1}
-	resultado := app.Calculate(array)
-	for i := 0; i < len(resultado); i++ {
-		fmt.Printf("%.6f \n", resultado[i])
+	for i := range array[0:] {
+		fmt.Println(array[i] + 1)
 	}
 
 }

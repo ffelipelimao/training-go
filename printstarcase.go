2991098117d26637a68ed4a366cec54144059e82
Luiz Felipe Limão <lflimao@hotmail.com>
2021-08-24 16:19:02 -0300

    Update printstarcase.go

--- printstarcase.go
+++ printstarcase.go
@@ -10,6 +10,7 @@ func main() {
 }
 
 func printStaircase(n int32) {
+	//para converter o tipo, só passar o nome como função
 	for i := int32(1); i <= n; i++ {
 		fmt.Printf("%s%s\n",
 			strings.Repeat(" ", int(n-i)),
